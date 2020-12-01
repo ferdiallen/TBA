d3ed6caca3df13eac8b2952b1a9cff8b09af749e
ferdiallen <60228890+ferdiallen@users.noreply.github.com>
2020-12-02 05:43:24 +0700

    Initial state

---
+++ KotlinVoice/src/Main/Main.kt
@@ -0,0 +1,19 @@
+package Main
+
+import java.util.*
+
+fun main(){
+    var v:String = " "
+
+    println("Masukkan Value")
+    v= readLine().toString()
+    println("Hasilnya: $v")
+    var z:CharArray = v.toCharArray()
+    println(z.contentToString())
+    println(z)
+    var hasil:String = z.joinToString("")
+
+    hasil=hasil.replace("\\s".toRegex(),"")
+    val newstr = hasil.chunked(2).joinToString("-")
+    println(newstr)
+}
